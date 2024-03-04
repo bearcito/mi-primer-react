6409b423294aa85b430ff752dc2424c1a5cae4a7
Nahuel Cabral <151670293+bearcito@users.noreply.github.com>
2024-03-04 15:45:07 -0300

    fixederror

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import CardPerson from './pages/CardPerson';
 
 function App() {
   return (
     <div className="App">
-      <CardPerson />
+      <h1>
+        Hello Dojo
+      </h1>
+      <h3>
+        things i need to do 
+      </h3>
+      <ul>
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li>
+      </ul>
     </div>
   );
 }
